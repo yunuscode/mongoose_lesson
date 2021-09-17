5eec95eae4fc1713282de0f90c38622d1ef19890
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-09-17 20:53:30 +0500

    Add login page

--- src/controllers/UserRouteController.js
+++ src/controllers/UserRouteController.js
@@ -2,4 +2,7 @@ module.exports = class UserRouteController {
 	static async UserRegistrationGetController(req, res) {
 		res.render("registration");
 	}
+	static async UserLoginGetController(req, res) {
+		res.render("login");
+	}
 };
--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,10 +1,12 @@
 const {
 	UserRegistrationGetController,
+	UserLoginGetController,
 } = require("../controllers/UserRouteController");
 
 const router = require("express").Router();
 
 router.get("/signup", UserRegistrationGetController);
+router.get("/login", UserLoginGetController);
 
 module.exports = {
 	path: "/users",
