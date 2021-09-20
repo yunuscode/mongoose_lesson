7ab592b9be45406c1a8c436f171f5460ea5d4638
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-09-20 20:52:09 +0500

    Add home page

--- src/controllers/HomeRouteController.js
+++ src/controllers/HomeRouteController.js
@@ -1,10 +1,11 @@
 const users = require("../models/UserModel");
+const categories = require("../models/CategoryModel");
 
 module.exports = class HomeRouteController {
 	static async HomeGetController(req, res) {
-		console.log(req.user);
 		res.render("index", {
 			user: req.user,
+			categories: await categories.find(),
 		});
 	}
 };
--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ async function server(mode) {
 		app.use(express.json());
 		app.use(express.urlencoded({ extended: true }));
 		app.use(cookieParser());
-		app.use(express.static(path.join(__dirname, "src", "public")));
+		app.use("/public", express.static(path.join(__dirname, "public")));
 		app.use(UserMiddleware);
 
 		await mongo();
